3e75f20afd56ed6a06c530a2ffe6dfb0668e1e08
Varun <varunshiri99@gmail.com>
2023-01-22 02:12:59 -0500

    Change analytics

--- src/components/Analytics.js
+++ src/components/Analytics.js
@@ -6,14 +6,6 @@ import { Chart, registerables } from "chart.js"
 import { Pie } from "react-chartjs-2";
 import Stack from '@mui/material/Stack';
 import Divider from '@mui/material/Divider';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import Box from "@mui/material/Box";
 import apiClient from "../clients/api-client";
 import { useEffect, useState } from "react";
 
