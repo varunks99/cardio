dbeefb33e28d5791a93c859e8ecf2d3851d53c8c
b44ken <bradboratto@gmail.com>
2023-01-21 20:37:33 -0500

    works but css is digusting

--- src/App.js
+++ src/App.js
@@ -4,20 +4,29 @@ import SignUp from './components/SignUp/SignUp';
 import SignIn from './components/SignIn/Sign';
 
 
+
 import { Navigation } from './components/Navigation';
 import { Earnings } from './components/Earnings';
 import { Home } from './components/Home';
 import { Analytics } from './components/Analytics';
 import { useState } from 'react';
+import { Box } from '@mui/material';
 
 function App() {
   const [tab, setTab] = useState(1)
 
   return <>
-      { (tab == 0) && <Earnings /> }
-      { (tab == 1) && <Home /> }
-      { (tab == 2) && <Analytics /> }
-      <Navigation tab={tab} setTab={setTab} />
+    <Box sx={ {
+      height: "90vh",
+      overflowY: "scroll"
+    } }>
+        { (tab == 0) && <Earnings /> }
+        { (tab == 1) && <Home /> }
+        { (tab == 2) && <Analytics /> }
+    </Box>
+    <Navigation tab={tab} setTab={setTab} sx={{
+      height: "10vh"
+    }} />
   </>
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,92 @@
+import { Box, List, ListItem, ListItemText } from "@mui/material"
+
+const Transaction = ({ type, date, name, amount }) => {
+    return <ListItem
+        key={type+date+name+amount}
+        sx={{ textAlign: "center",
+    }}
+    >
+        <ListItemText primary={name} secondary={date + ", " + amount} />
+
+    </ListItem>
+}
+
 export const Home = () => {
-    return <h1>Home tab</h1>
+    const hardcodeTransactions = [
+        { 
+            type: "Payment",
+            date: "Jan 12",
+            name: "Pizza",
+            amount: "$15.00"
+        },
+        { 
+            type: "Payment",
+            date: "Jan 12",
+            name: "Pizza",
+            amount: "$15.00"
+        },
+        { 
+            type: "Payment",
+            date: "Jan 12",
+            name: "Pizza",
+            amount: "$15.00"
+        },
+        { 
+            type: "Payment",
+            date: "Jan 12",
+            name: "Pizza",
+            amount: "$15.00"
+        },
+        { 
+            type: "Payment",
+            date: "Jan 12",
+            name: "Pizza",
+            amount: "$15.00"
+        },
+        { 
+            type: "Payment",
+            date: "Jan 12",
+            name: "Pizza",
+            amount: "$15.00"
+        },
+        { 
+            type: "Payment",
+            date: "Jan 12",
+            name: "Pizza",
+            amount: "$15.00"
+        },
+        { 
+            type: "Payment",
+            date: "Jan 12",
+            name: "Pizza",
+            amount: "$15.00"
+        },
+        { 
+            type: "Payment",
+            date: "Jan 12",
+            name: "Pizza",
+            amount: "$15.00"
+        },
+        { 
+            type: "Payment",
+            date: "Jan 12",
+            name: "Pizza",
+            amount: "$15.00"
+        },
+        
+    ]
+
+    return  <Box sx={{
+        mx: "auto", 
+        width: 450,
+        textAlign: "center"
+    }}>
+        <h1>Home</h1>
+        <h1>Balance: $250.00</h1>
+        <List>
+            { hardcodeTransactions.map(data => 
+                <Transaction type={data.type} date={data.date} name={data.name} amount={data.amount} />
+            ) }
+        </List>
+    </Box>
 }
