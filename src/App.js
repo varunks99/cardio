840365e27af79272630ecf836c5af98f563925e6
Varun <varunshiri99@gmail.com>
2023-01-21 20:02:07 -0500

    Sign up with API integrated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes, Navigate, Outlet, BrowserRouter } from 'react-router-dom';
+import SignUp from './components/SignUp/SignUp';
+import SignIn from './components/SignIn/Sign';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
+          {/* <Route path="/" element={<PrivateOutlet />}>
+            <Route path="/" element={<Home user={userData || {}} />} />
+            <Route path="/statistics" element={<Statistics theme={theme} userRegion={userData.statsRegion} />} />
+            <Route path="/calculator" element={<Calculator user={userData || {}} />} />
+            <Route path="/news" element={<News theme={theme} />} />
+            <Route path="/account" element={<Account colorMode={colorMode} theme={theme} user={userData || {}} />} />
+            <Route path="/settings" element={<Settings />} />
+          </Route> 
+          <Route path="*" element={<PageNotFound />} />*/}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
